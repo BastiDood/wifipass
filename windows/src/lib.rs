ae60487633069ece820597fedee2e5a61a0474a0
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-02-03 19:21:44 +0800

    refactor: prefer caching references over pointers

--- windows/src/lib.rs
+++ windows/src/lib.rs
@@ -13,41 +13,54 @@ pub struct Wlan {
     handle: HANDLE,
 }
 
-pub struct WlanInterfaces {
-    interfaces: *mut WiFi::WLAN_INTERFACE_INFO_LIST,
+pub struct WlanInterfaceList {
+    interfaces: &'static WiFi::WLAN_INTERFACE_INFO_LIST,
 }
 
-impl WlanInterfaces {
+impl WlanInterfaceList {
+    /// # Safety
+    /// Takes ownership of `interfaces`.
+    pub unsafe fn try_new(interfaces: *const WiFi::WLAN_INTERFACE_INFO_LIST) -> Option<Self> {
+        let interfaces = interfaces.as_ref()?;
+        Some(Self { interfaces })
+    }
+
     pub fn as_slice(&self) -> Option<&[WiFi::WLAN_INTERFACE_INFO]> {
-        let WiFi::WLAN_INTERFACE_INFO_LIST { InterfaceInfo, dwNumberOfItems, .. } =
-            unsafe { self.interfaces.as_ref() }?;
-        let count = (*dwNumberOfItems).try_into().unwrap();
+        let WiFi::WLAN_INTERFACE_INFO_LIST { ref InterfaceInfo, dwNumberOfItems, .. } = *self.interfaces;
+        let count = dwNumberOfItems.try_into().unwrap();
         Some(unsafe { core::slice::from_raw_parts(InterfaceInfo.as_ptr(), count) })
     }
 }
 
-impl Drop for WlanInterfaces {
+impl Drop for WlanInterfaceList {
     fn drop(&mut self) {
-        let interfaces = core::mem::replace(&mut self.interfaces, core::ptr::null_mut()).cast();
+        let interfaces = core::ptr::from_ref(self.interfaces).cast();
         unsafe { WiFi::WlanFreeMemory(interfaces) };
     }
 }
 
 pub struct WlanProfileList {
-    profiles: *mut WiFi::WLAN_PROFILE_INFO_LIST,
+    profiles: &'static WiFi::WLAN_PROFILE_INFO_LIST,
 }
 
 impl WlanProfileList {
+    /// # Safety
+    /// Takes ownership of `interfaces`.
+    pub unsafe fn try_new(interfaces: *const WiFi::WLAN_PROFILE_INFO_LIST) -> Option<Self> {
+        let profiles = interfaces.as_ref()?;
+        Some(Self { profiles })
+    }
+
     pub fn as_slice(&self) -> Option<&[WiFi::WLAN_PROFILE_INFO]> {
-        let WiFi::WLAN_PROFILE_INFO_LIST { ProfileInfo, dwNumberOfItems, .. } = unsafe { self.profiles.as_ref() }?;
-        let count = (*dwNumberOfItems).try_into().unwrap();
+        let WiFi::WLAN_PROFILE_INFO_LIST { ref ProfileInfo, dwNumberOfItems, .. } = *self.profiles;
+        let count = dwNumberOfItems.try_into().unwrap();
         Some(unsafe { core::slice::from_raw_parts(ProfileInfo.as_ptr(), count) })
     }
 }
 
 impl Drop for WlanProfileList {
     fn drop(&mut self) {
-        let profiles = core::mem::replace(&mut self.profiles, core::ptr::null_mut()).cast();
+        let profiles = core::ptr::from_ref(self.profiles).cast();
         unsafe { WiFi::WlanFreeMemory(profiles) };
     }
 }
@@ -86,18 +99,20 @@ impl Wlan {
         Ok(Self { handle })
     }
 
-    pub fn enum_interfaces(&self) -> Result<WlanInterfaces> {
-        let mut interfaces = core::ptr::null_mut();
-        let result = unsafe { WiFi::WlanEnumInterfaces(self.handle, None, &mut interfaces) };
+    pub fn enum_interfaces(&self) -> Result<WlanInterfaceList> {
+        let mut output = core::ptr::null_mut();
+        let result = unsafe { WiFi::WlanEnumInterfaces(self.handle, None, &mut output) };
         WIN32_ERROR(result).ok()?;
-        Ok(WlanInterfaces { interfaces })
+        let interfaces = unsafe { WlanInterfaceList::try_new(output) };
+        Ok(interfaces.unwrap())
     }
 
     pub fn get_profile_list(&self, interface_guid: &GUID) -> Result<WlanProfileList> {
-        let mut profiles = core::ptr::null_mut();
-        let result = unsafe { WiFi::WlanGetProfileList(self.handle, interface_guid, None, &mut profiles) };
+        let mut output = core::ptr::null_mut();
+        let result = unsafe { WiFi::WlanGetProfileList(self.handle, interface_guid, None, &mut output) };
         WIN32_ERROR(result).ok()?;
-        Ok(WlanProfileList { profiles })
+        let profiles = unsafe { WlanProfileList::try_new(output) };
+        Ok(profiles.unwrap())
     }
 
     pub fn get_profile(&self, interface_guid: &GUID, profile_name: PCWSTR) -> Result<WlanProfileXml> {
